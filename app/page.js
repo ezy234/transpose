8524ae2c1fa6e34593946fb378fd86bfe183405a
IsraelIyke <nwangwuisrael@gmail.com>
2024-02-05 00:25:35 +0100

    almost done

--- app/page.js
+++ app/page.js
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <nav>nav</nav>
+      <div className="sub-container">
+        <div className="banner">banner</div>
+        <div className="card">
+          <div>card</div>
+        </div>
+        <div className="card-2">
+          <div>card</div>
+          <div>card</div>
+        </div>
+        <div className="card-3">
+          <div>card</div>
+          <div>card</div>
+          <div>card</div>
+        </div>
+      </div>
+    </div>
+  );
 }
